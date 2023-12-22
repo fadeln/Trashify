939ee5c5b3405ede729e5ed687a29eaffd893920
fadel najmi adliansyah <fadelnajmiadliansyah@gmail.com>
2023-12-22 15:46:22 +0700

    fix mobile dev

--- Mobile Development/app/src/main/java/com/example/trashify/data/retrofit/ApiConfig.kt
+++ Mobile Development/app/src/main/java/com/example/trashify/data/retrofit/ApiConfig.kt
@@ -6,7 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 object ApiConfig {
-    fun getApiService(token: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLWRJLTgyVENTb0tpbk1uUTYiLCJpYXQiOjE3MDMwOTA5NDR9.MMFJx06LQ_ndzETprqTx-kOMB7cXKojY5WhZmuY8xNM"): ApiService {
+    fun getApiService(token: String? = null ): ApiService {
         val loggingInterceptor =
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         val authInterceptor = Interceptor{ chain ->
@@ -42,7 +42,7 @@ object RetrofitInstance {
 }
 
 object ApiConfigPost {
-    fun getApiServicePost(token: String? = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLWRJLTgyVENTb0tpbk1uUTYiLCJpYXQiOjE3MDMwNzIxMTZ9.8lBfpa_dJVBXXcg5DBnoZ2vJPzdsHbu1HVC5FnGp0P8"): ApiServicePost {
+    fun getApiServicePost(token: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLWRJLTgyVENTb0tpbk1uUTYiLCJpYXQiOjE3MDMwOTA5NDR9.MMFJx06LQ_ndzETprqTx-kOMB7cXKojY5WhZmuY8xNM"): ApiServicePost {
         val loggingInterceptor =
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         val authInterceptor = Interceptor{ chain ->
